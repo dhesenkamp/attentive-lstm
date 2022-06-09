3e77a2c8a170af82e8ae39ccb5cee18ee888e20c
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-06-09 17:32:23 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras import Model
+
+
+class AttentiveLSTM(Model):
+
+
+    def __init__(self):
+        pass
+
+
+    def call(self, input):
+        pass
+    
