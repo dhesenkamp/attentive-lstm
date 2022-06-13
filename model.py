e0901b509b5181290b3f8680f2b7ec3001f6eef5
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-06-13 12:18:16 +0100

    Update batch norm layer
    Used wrong layer

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from tensorflow.keras import Model
-from tf.keras.layers import Dense, LSTM, Bidirectional, BatchNorm, Dropout
+from tf.keras.layers import Dense, LSTM, Bidirectional, BatchNormalization, Dropout
 from tensorflow_addons.rnn import LayerNormLSTMCell
-from attention import SelfAttention
+from attention import SimpleAttention, SelfAttention
 
 
 class AttentiveLSTM(Model):
+    """Self-attentive LSTM based on Coskun et al. (2018)"""
 
 
     def __init__(self, hidden_units=128):
@@ -13,25 +14,25 @@ class AttentiveLSTM(Model):
 
         self.attention_layer = SelfAttention()
         self.all_layers = [
-            Bidirectional(LSTM(units=hidden_units)),
+            Bidirectional(LSTM(units=hidden_units, return_sequences=True)), #output of shape seq_len x hidden_units*2(bidir)
             # possibly use this instead
             #LayerNormLSTMCell(units=hidden_units)
 
-            BatchNorm(),
+            BatchNormalization(),
             Dropout(),
             
             #self-attention
             self.attention_layer(),
 
-            BatchNorm(),
+            BatchNormalization(),
             Dropout(),
             Dense(units=320, activation='relu'),
 
-            BatchNorm(),
+            BatchNormalization(),
             Dropout(),
             Dense(units=320, activation='relu'),
 
-            BatchNorm(),
+            BatchNormalization(),
             Dense(units=128),
 
             #l2 norm 
