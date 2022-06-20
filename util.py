64847d1588b1a3f070bdcfca784f3d3ad42fa1c4
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-06-20 14:03:32 +0100

    Add MMD function

--- util.py
+++ util.py
@@ -115,3 +115,22 @@ def kernel_function(x, x_prime, sigma=[1,2,4,8,16]):
     k_sum = tf.math.reduce_sum(k)
     
     return k_sum.numpy()
+
+
+def mmd(X, Y):
+    """
+    Calculate maximum mean discrepancy between distributions X and Y.
+    See Coskun et al. (2018), chapter 4 'Loss Function' for details, esp. formula 7.
+
+    Args:
+        X (array): samples from distribution X 
+        Y (array): samples from distribution Y
+    Returns:
+        mmd (float) = maximum mean discrepancy
+    """
+    first_term = tf.math.divide(1, len(X)**2) * [kernel_function(i, j) for i in X for j in X]
+    second_term = tf.math.divide(2, len(X)*len(Y)) * [kernel_function(i, j) for i in X for j in Y]
+    third_term = tf.math.divide(1, len(Y)**2) * [kernel_function(i, j) for i in Y for j in Y]
+
+    mmd = tf.math.reduce_sum(first_term) - tf.math.reduce_sum(second_term) + tf.math.reduce_sum(third_term)
+    return mmd.numpy()
