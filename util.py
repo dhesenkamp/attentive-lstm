06d2cf0eccc50f09fc4bed64e74e8e682771576a
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-06-27 11:38:18 +0100

    Changed indexing to work with dicts

--- util.py
+++ util.py
@@ -14,7 +14,8 @@ def create_batch(df, num_classes=5, samples=25):
     Args:
         df: dataframe from which to create dataset (json)
         num_classes (int): number of classes to consider for negative examples. Default: 5
-        samples (int): number of samples per category. Default: 25
+        samples (int): number of samples per category. Ultimately corresponds to the batch size, as this
+            is how many samples will be fed to the model per epoch. Default: 25
     Returns:
         batch (np.array): batch with (num_classes+2 * samples) many samples
     """
@@ -48,47 +49,47 @@ def create_batch(df, num_classes=5, samples=25):
     
     # create array of 25 random samples, repeat for all selected classes
     # anchor class
-    arr = np.arange(df[pos_class].shape[0]) # get indices for samples of this class
+    arr = np.arange(len(df[pos_class])) # get indices for samples of this class
     np.random.shuffle(arr) # shuffle
     anchor = [df[pos_class][arr[i]] for i in range(samples)] # select 25 samples
     batch.append(anchor)
     
     # positive class
-    arr = np.arange(df[pos_class].shape[0])
+    arr = np.arange(len(df[pos_class]))
     np.random.shuffle(arr)
     pos = [df[pos_class][arr[i]] for i in range(samples)]
     batch.append(pos)
 
     # negative class 1    
-    arr = np.arange(df[neg_class_1].shape[0])
+    arr = np.arange(len(df[neg_class_1]))
     np.random.shuffle(arr)
     neg_1 = [df[neg_class_1][arr[i]] for i in range(samples)]
     batch.append(neg_1)
 
     # negative class 2
-    arr = np.arange(df[neg_class_2].shape[0])
+    arr = np.arange(len(df[neg_class_2]))
     np.random.shuffle(arr)
     neg_2 = [df[neg_class_2][arr[i]] for i in range(samples)]
     batch.append(neg_2)
 
     # negative class 3
-    arr = np.arange(df[neg_class_3].shape[0])
+    arr = np.arange(len(df[neg_class_3]))
     np.random.shuffle(arr)
     neg_3 = [df[neg_class_3][arr[i]] for i in range(samples)]
     batch.append(neg_3)
     
     # negative class 4
-    arr = np.arange(df[neg_class_4].shape[0])
+    arr = np.arange(len(df[neg_class_4]))
     np.random.shuffle(arr)
     neg_4 = [df[neg_class_4][arr[i]] for i in range(samples)]
     batch.append(neg_4)
     
     # negative class 5
-    arr = np.arange(df[neg_class_5].shape[0])
+    arr = np.arange(len(df[neg_class_5]))
     np.random.shuffle(arr)
     neg_5 = [df[neg_class_5][arr[i]] for i in range(samples)]
     batch.append(neg_5)
     
     # batch now has shape [nr_classes, nr_samples, feat_vector]
-    # feat_vector itself should have shape [time_steps, nr_joints, coordinates]
+    # feat_vector itself should have shape [time_steps, nr_joints * coordinates]
     return batch
