cc849b4c19d39b67b043a314b4627f66ded6e9e1
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-07-11 13:51:16 +0100

    Custom top layer for classification
    Added an optional flatten operation and top layer to make model usable for classification

--- model.py
+++ model.py
@@ -11,17 +11,34 @@ class AttentiveLSTM(Model):
     Currently with triplet semihard loss as MMD-NCA is not implemented yet.
     """
 
-    def __init__(self, hidden_units=128, r=5):
+    def __init__(self, hidden_units=128, r=5, d=10, classification=False, **kwargs):
         """
         Constructor
         
         Args:
             hidden_units (int): number of hidden units for the LSTM, i.e. size of embedding
             r (int): number of frames in the motion sequence which the model should "pay attention" to
+            d (int): determines size of attention layer weight matrices
+            classification (Bool): whether to include a top layer for classification
+        
+        Keyword args:
+            nr_classes (int): number of classes for classification task. Determines number of units in top layer.
+                Only necessary when classification==True
         """
         super(AttentiveLSTM, self).__init__()
 
-        self.optimizer = tf.keras.optimizers.SGD(learning_rate=0.0001, momentum=0.9) #decay missing
+        self.classification = classification
+
+        self.lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=0.0001,
+            decay_steps=50,
+            decay_rate=0.96
+        )
+        self.optimizer = tf.keras.optimizers.SGD(
+            learning_rate=self.lr_schedule, 
+            momentum=0.9,
+            global_clipnorm=25
+            )
         self.dropout_rate = 0.5
 
         self.bi_lstm = Bidirectional(LSTM(units=hidden_units, return_sequences=True))
@@ -31,8 +48,7 @@ class AttentiveLSTM(Model):
         self.dropout1 = Dropout(rate=self.dropout_rate)
 
         # attention layer
-        #self.simpleAttention = SimpleAttention()
-        self.selfAttention = SelfAttention(r=r, lstm_units=hidden_units)
+        self.selfAttention = SelfAttention(r=r, d=d, lstm_units=hidden_units)
 
         self.batchnorm2 = BatchNormalization()
         self.dropout2 = Dropout(rate=self.dropout_rate)
@@ -46,7 +62,12 @@ class AttentiveLSTM(Model):
         self.dense3 = Dense(units=128) #activation?
 
         # l2 normalization
-        self.l2 = Lambda(lambda x: tf.math.l2_normalize(x, axis=1))
+        self.l2 = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1))
+
+        # flatten for classification
+        if self.classification==True:
+            self.flatten = tf.keras.layers.Flatten()
+            self.out = Dense(units=kwargs['nr_classes'], activation='softmax')
 
 
     def call(self, input, training=False):
@@ -78,5 +99,9 @@ class AttentiveLSTM(Model):
         x = self.dense3(x)
 
         x = self.l2(x)
+
+        if self.classification == True:
+            x = self.flatten(x)
+            x = self.out(x)
 
         return x
