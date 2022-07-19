007db6a7c3106cc0047f7f068cb843b14714fdcc
Dennis Hesenkamp <dhesenkamp@uos.de>
2022-07-19 17:34:51 +0100

    Model with functional API
    Added a function to create the model via TF's functional API for ease of use

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras import Model
-from tf.keras.layers import Dense, LSTM, Bidirectional, BatchNormalization, Dropout, Lambda
+from tf.keras.layers import Dense, LSTM, Bidirectional, BatchNormalization, Dropout, Lambda, Flatten, Input
 from attention import SimpleAttention, SelfAttention
 
 
@@ -105,3 +105,58 @@ class AttentiveLSTM(Model):
             x = self.out(x)
 
         return x
+
+
+def create_model(input_shape, hidden_units=128, dropout=0.5, r=5, d=10, final_embedding_size=128, classification=False, **kwargs):
+    """Create self-attentive BiLSTM.
+
+    Builds a self-attentive bidirectional LSTM for human motion classification using TensorFlow's functional API.
+    Model architecture based on Coskun et al. (2018).
+    
+    Args:
+        input_shape (tuple): Shape of single sequence. Should be (sequence length X nr of features).
+        hidden_units (int): Number of hidden units per LSTM. Default: 128
+        dropout (float): Dropout rate, equal among all dropout layers. Default: 0.5
+        r (int): Number of steps in sequence to pay attention to. Default: 5
+        d (int): Hyperparameter for weight matrix size in attention layer. Default: 10
+        final_embedding_size (int): Size of the final embedding of the sequence. Default: 128
+        classification (Bool): Whether to use the model in classification mode. If True, adds an additional top layer.
+            If True, also supply kwarg 'nr_classes'. Default: False.
+
+    Keyword Args:
+        nr_classes (int): Number of classes for classification. Determines number of units in top layer if used. 
+            Only required if classification=True.
+        
+    Returns:
+        model: TensorFlow model instance
+    """
+
+    input = Input(shape=input_shape)
+    x = Bidirectional(LSTM(units=hidden_units, return_sequences=True))(input)
+
+    x = BatchNormalization()(x)
+    x = Dropout(rate=dropout)(x)
+    x = SelfAttention(r=r, d=d, lstm_units=hidden_units)(x)
+
+    x = BatchNormalization()(x)
+    x = Dropout(rate=dropout)(x)
+    x = Dense(units=320, activation='leaky_relu')(x)
+
+    x = BatchNormalization()(x)
+    x = Dropout(rate=dropout)(x)
+    x = Dense(units=320, activation='leaky_relu')(x)
+
+    x = BatchNormalization()(x)
+    x = Dense(units=final_embedding_size)(x)
+
+    if classification == True:
+        x = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1))(x)
+        x = Flatten()(x)
+        output = Dense(units=kwargs['nr_classes'], activation='softmax')(x)
+    else:
+        x = Flatten()(x)
+        output = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1))(x)
+
+    model = Model(inputs=input, outputs=output)
+
+    return model
